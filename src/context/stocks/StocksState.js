982124e1195fd80f326aef02417f3a1c7d7ce237
Chris Bowling <chris2fer2002@gmail.com>
2020-04-04 14:04:50 -0400

    fixed some bugs

--- src/context/stocks/StocksState.js
+++ src/context/stocks/StocksState.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useReducer, useEffect, useRef } from 'react';
 import axios from 'axios';
 import StocksContext from './stocksContext';
 import StocksReducer from './stockReducer';
@@ -12,10 +12,10 @@ import {
   SHOW_HEADER,
   SET_TEXT,
   SEARCH_STOCKS,
-  UPDATE_STOCKS
+  UPDATE_STOCKS,
 } from '../types';
 
-const StocksState = props => {
+const StocksState = (props) => {
   const initialState = {
     stocks: [
       {
@@ -40,7 +40,7 @@ const StocksState = props => {
         pe: 19.896784,
         earningsAnnouncement: '2020-01-28T21:30:00.000+0000',
         sharesOutstanding: 4375479808,
-        timestamp: 1585321880
+        timestamp: 1585321880,
       },
       {
         symbol: 'FB',
@@ -64,8 +64,8 @@ const StocksState = props => {
         pe: 24.311045,
         earningsAnnouncement: '2020-01-29T21:05:04.000+0000',
         sharesOutstanding: 2405750016,
-        timestamp: 1585321880
-      }
+        timestamp: 1585321880,
+      },
     ],
     searchResults: null,
     editOpen: false,
@@ -73,18 +73,22 @@ const StocksState = props => {
     showHeaderNavbar: true,
     stateText: '',
     loading: false,
-    error: null
+    error: null,
   };
-
-  useEffect(() => {
-    setInterval(() => {updateStocks()}, 30000);
-    
-  }, []);
 
   const [state, dispatch] = useReducer(StocksReducer, initialState);
 
+  const stateRef = useRef(state.stocks)
+  stateRef.current = state.stocks;
+
+  useEffect(() => {
+    setInterval(() => {
+      updateStocks(stateRef.current)
+    }, 30000);
+  }, []);
+
   // Search Stocks
-  const searchStocks = async sym => {
+  const searchStocks = async (sym) => {
     // await data from API
     const res = await axios.get(
       `https://financialmodelingprep.com/api/v3/search?query=${sym}&exchange=NASDAQ`
@@ -95,52 +99,58 @@ const StocksState = props => {
     );
 
     // after data has returned in variables, then filter results
-    const filteredResults = [...res.data, ...res2.data].filter(stock =>
+    const filteredResults = [...res.data, ...res2.data].filter((stock) =>
       stock.symbol.includes(sym.toUpperCase())
     );
-
-    // console.log(state.text);
-    console.log(filteredResults);
 
     // dispatch to have filtered results added to state to show the user
     dispatch({
       type: SEARCH_STOCKS,
-      payload: filteredResults
+      payload: filteredResults,
     });
   };
 
   // Clear Results
   const clearResults = () => {
     dispatch({
-      type: CLEAR_RESULTS
+      type: CLEAR_RESULTS,
     });
   };
 
   // Add Stock
-  const addStock = async sym => {
-    const res = await axios.get(
-      `https://financialmodelingprep.com/api/v3/quote/${sym}`
-    );
-
-    dispatch({
-      type: ADD_STOCK,
-      payload: res.data[0]
-    });
-
+  const addStock = async (sym) => {
+    let inArray = false;
+    // Searchs if Company is already in the stocks array
+    state.stocks.forEach(stock => {
+      if(stock.symbol === sym) {
+        inArray = true;
+      }
+    })
+    if(!inArray) {
+      const res = await axios.get(
+        `https://financialmodelingprep.com/api/v3/quote/${sym}`
+      );
+  
+      dispatch({
+        type: ADD_STOCK,
+        payload: res.data[0],
+      });
+    }
     showHeader(true);
   };
 
   // Delete Stock
-  const deleteStock = sym => {
+  const deleteStock = (sym) => {
     dispatch({
       type: DELETE_STOCK,
-      payload: sym
+      payload: sym,
     });
   };
 
   // Update Stocks
-  const updateStocks = async () => {
-    const res = await state.stocks.map(async stock => {
+  const updateStocks = async (stocks) => {
+
+    const res = await stocks.map(async (stock) => {
       let response = await axios.get(
         `https://financialmodelingprep.com/api/v3/quote/${stock.symbol}`
       );
@@ -148,48 +158,48 @@ const StocksState = props => {
       return response;
     });
 
-    Promise.all(res).then(response => {
-      const stockPayload = response.map(stock => {
+    Promise.all(res).then((response) => {
+      const stockPayload = response.map((stock) => {
         return stock.data[0];
       });
-      console.log(stockPayload);
+      // console.log(stockPayload);
       dispatch({
         type: UPDATE_STOCKS,
-        payload: stockPayload
+        payload: stockPayload,
       });
     });
   };
 
   // Set Text
-  const setStateText = text => {
+  const setStateText = (text) => {
     dispatch({
       type: SET_TEXT,
-      payload: text
+      payload: text,
     });
   };
 
   // Edit Toggle
   const editToggle = () => {
     dispatch({
-      type: EDIT_TOGGLE
+      type: EDIT_TOGGLE,
     });
   };
 
   // Changes/ChangesPercentage Toggle Handler
   const changeBoolHandler = () => {
     dispatch({
-      type: CHANGE_TOGGLE
+      type: CHANGE_TOGGLE,
     });
   };
 
   // Toggle Focus
-  const showHeader = bool => {
+  const showHeader = (bool) => {
     if (bool) {
       clearResults();
     }
     dispatch({
       type: SHOW_HEADER,
-      payload: bool
+      payload: bool,
     });
   };
 
@@ -216,7 +226,7 @@ const StocksState = props => {
         showHeader,
         setStateText,
         searchStocks,
-        updateStocks
+        updateStocks,
       }}
     >
       {props.children}
